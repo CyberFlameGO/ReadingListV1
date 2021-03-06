3d91f10da7190e5ac1ef7bb41193d846857c5fda
Andrew Bennet <me@andrewbennet.com>
2021-03-06 22:53:26 +0000

    Reorder event log and operation

--- ReadingList/ViewControllers/ImportExport/ImportExport.swift
+++ ReadingList/ViewControllers/ImportExport/ImportExport.swift
@@ -63,8 +63,8 @@ final class ImportExport: UITableViewController {
         // The CONFIRM DELETE action:
         let confirmDelete = UIAlertController(title: "Final Warning", message: "This action is irreversible. Are you sure you want to continue?", preferredStyle: .alert)
         confirmDelete.addAction(UIAlertAction(title: "Delete", style: .destructive) { _ in
-            DeleteAllManager.shared.deleteAll()
             UserEngagement.logEvent(.deleteAllData)
+            DeleteAllManager.shared.deleteAll()
         })
         confirmDelete.addAction(UIAlertAction(title: "Cancel", style: .cancel))
 
