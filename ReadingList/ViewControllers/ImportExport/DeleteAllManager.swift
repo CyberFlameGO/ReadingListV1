0eb3e800b021a03a17009293938197c98a060977
Andrew Bennet <me@andrewbennet.com>
2021-03-06 20:48:39 +0000

    Attempt to fix issue with syncCoordinator combined with full data deletion

--- ReadingList/ViewControllers/ImportExport/DeleteAllManager.swift
+++ ReadingList/ViewControllers/ImportExport/DeleteAllManager.swift
@@ -10,18 +10,27 @@ class DeleteAllManager {
     /// Switch to the deletion screen, delete the persistent store, and then switch back to the app's normal root controller when complete.
     func deleteAll() {
         guard let window = AppDelegate.shared.window else { fatalError("No window available when attempting to delete all") }
-        os_log("Replacing window root view controller with deletion placeholder view")
+        logger.info("Replacing window root view controller with deletion placeholder view")
         window.rootViewController = DeleteAll()
 
         DispatchQueue.global(qos: .userInitiated).async {
-            os_log("Destroying persistent store")
+            logger.info("Destroying persistent store")
             PersistentStoreManager.container.persistentStoreCoordinator.destroyAndDeleteStore(at: PersistentStoreManager.storeLocation)
 
-            os_log("Initialising persistent store")
+            if let syncCoordinator = AppDelegate.shared.syncCoordinator {
+                syncCoordinator.stop()
+                AppDelegate.shared.syncCoordinator = nil
+                CloudSyncSettings.settings.syncEnabled = false
+            }
+
+            logger.info("Initialising persistent store")
             try! PersistentStoreManager.initalisePersistentStore {
 
-                os_log("Persistent store initialised; replacing window root view controller")
+                logger.info("Reinitialising persistent store coordinator")
+                AppDelegate.shared.initialiseSyncCoordinator()
+
                 DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    logger.info("Replacing window root view controller")
                     let newTabBarController = TabBarController()
                     window.rootViewController = newTabBarController
                     newTabBarController.presentImportExportView(importUrl: nil)
